3ed602130122c5a2e51c4ec701853bc9a23bd826
Edwin T <quantum.explorer@gmail.com>
2019-03-05 10:53:59 -0500

    Added Clifford-Pauli commutation ruleset.

--- experimental/stabilizerQC.py
+++ experimental/stabilizerQC.py
@@ -1,3 +1,5 @@
+# gatelut = ['h','cx','cz','s']
+
 def pauli_merge(p1, p2):
     '''
     Compose two Pauli operators encoded as integers.
@@ -24,3 +26,53 @@ def pauli_demux(nqb, p1):
         p1 = quot
     
     return plist
+
+def pauli_commute(pvec, op, qargs):
+    '''Ruleset for propagation of Pauli operators through Clifford gates.
+    Integer "op" numbers correspond to Clifford gates as listed in gatelut.'''
+    if op==0:
+        qb = qargs[0]
+        if pvec[qb]==1:
+            pvec[qb] = 2
+        elif pvec[qb]==2:
+            pvec[qb] = 1
+    
+    elif op==1:
+        src = qargs[0]
+        trg = qargs[1]
+
+        if pvec[src]%2==1: # If there is an X Pauli at control qb
+            newtrg = pauli_merge(pvec[trg], 1)
+        else:
+            newtrg = pvec[trg]
+        
+        if pvec[trg]//2==1: # If there is a Z Pauli at target qb
+            newsrc = pauli_merge(pvec[src], 2)
+        else:
+            newsrc = pvec[src]
+        
+        pvec[src] = newsrc
+        pvec[trg] = newtrg
+    
+    elif op==2:
+        src = qargs[0]
+        trg = qargs[1]
+
+        if pvec[src]%2==1:
+            newtrg = pauli_merge(pvec[trg], 2)
+        else:
+            newtrg = pvec[trg]
+        
+        if pvec[trg]%2==1:
+            newsrc = pauli_merge(pvec[src], 2)
+        else:
+            newsrc = pvec[src]
+
+        pvec[src] = newsrc
+        pvec[trg] = newtrg
+
+    elif op==3: 
+        zbit = pvec[qargs[0]]//2
+        xbit = pvec[qargs[0]]%2
+        zbit = (zbit + xbit)%2
+        pvec[qargs[0]] = 2*zbit + xbit
